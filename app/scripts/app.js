b3a8493fef2925702c8d5e65f5f8f928f62d1d84
Matías Hernández Arellano <matiasfh@gmail.com>
2014-05-01 20:20:59 -0400

    base html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,12 +8,12 @@ angular
     'ngRoute'
   ])
   .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/main.html',
+    //     controller: 'MainCtrl'
+    //   })
+    //   .otherwise({
+    //     redirectTo: '/'
+    //   });
   });
